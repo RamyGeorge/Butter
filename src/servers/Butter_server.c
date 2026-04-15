6105c88876d91acdb7709bd2f0e0bde4df889e58
RamyGeorge <ramygeorge19@gmail.com>
2026-04-16 00:43:42 +0200

    server

---
+++ src/servers/Butter_server.c
@@ -0,0 +1,36 @@
+#include "Butter_server.h"
+#include <netdb.h>
+#include <sys/socket.h>
+
+
+
+int butter_start_serv(const char *port, int family,int protocol,int* fd){
+    struct addrinfo hints;
+    memset(&hints,0,sizeof(struct addrinfo));
+    hints.ai_family = family ? (family > 1 ? AF_INET6 :AF_INET): AF_UNSPEC;
+    hints.ai_socktype = protocol ? SOCK_DGRAM : SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+    struct addrinfo* bindaddr;
+    if (getaddrinfo(NULL, port, &hints, &bindaddr)){
+        fprintf(stderr,"service translation failed ..\n");
+        freeaddrinfo(bindaddr);
+        return 1;
+    }
+    int server = socket(bindaddr->ai_family, bindaddr->ai_socktype, bindaddr->ai_protocol); 
+    if(server < 0){
+        fprintf(stderr,"Creating a socket failed .. ");
+        freeaddrinfo(bindaddr);
+        return 1;
+    }
+    if(bind(server, bindaddr->ai_addr, bindaddr->ai_addrlen)){
+        fprintf(stderr,"socket binding failed.. \n");
+        close(server);
+        freeaddrinfo(bindaddr);
+        return 1;
+    }
+    freeaddrinfo(bindaddr);
+    *fd = server;
+    return 0;
+    
+
+}
---
+++ src/servers/Butter_server.h
@@ -0,0 +1,5 @@
+#include "../Socket_headers.h"
+
+
+int butter_start_serv(const char* port,int family,int protocol,int* returnsock);
+int butter_pop_client();
